cb89b9bf4c5a9942a7665eae833d1f02017a19b5
Kyle Tolle <kyle@nullsix.com>
2014-11-04 12:52:23 -0700

    Add log locations association to log

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,7 @@
 class Log < ActiveRecord::Base
   validates_presence_of :entry_name
+
+  has_many :log_locations, table_name: :logs_locations,
+      foreign_key: :log_id, dependent: :destroy
 end
 
