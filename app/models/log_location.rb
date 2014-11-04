aad990a10342ff058c0319fc6409a5c4a2b5faea
Kyle Tolle <kyle@nullsix.com>
2014-11-04 13:28:02 -0700

    Destroy custom locations on log location destroy

--- app/models/log_location.rb
+++ app/models/log_location.rb
@@ -1,9 +1,16 @@
 class LogLocation < ActiveRecord::Base
   self.table_name = "logs_locations"
 
+  after_destroy :destroy_custom_location
+
   belongs_to :log
   belongs_to :location, polymorphic: true
 
   accepts_nested_attributes_for :location
+
+private
+  def destroy_custom_location
+    location.destroy if location.is_a?(CustomLocation)
+  end
 end
 
