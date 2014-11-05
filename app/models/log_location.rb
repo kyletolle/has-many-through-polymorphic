3b9ed3900bcc51fbe0d61537aaaf995d100a5dcb
Kyle Tolle <kyle@nullsix.com>
2014-11-04 19:29:39 -0700

    Validate the location type
    Can only add Country and CustomLocation as a type of location.

--- app/models/log_location.rb
+++ app/models/log_location.rb
@@ -1,6 +1,8 @@
 class LogLocation < ActiveRecord::Base
   self.table_name = "logs_locations"
 
+  validate  :valid_location
+
   after_destroy :destroy_custom_location
 
   belongs_to :log
@@ -9,6 +11,15 @@ class LogLocation < ActiveRecord::Base
   accepts_nested_attributes_for :location
 
 private
+  def valid_location
+    location_is_valid_type =
+      [CustomLocation, Country].include?(location.class)
+
+    unless location_is_valid_type
+      errors.add :location, "must be a type of location (Country, CustomLocation)"
+    end
+  end
+
   def destroy_custom_location
     location.destroy if location.is_a?(CustomLocation)
   end
